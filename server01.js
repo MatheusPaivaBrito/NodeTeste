05cde81c695d0f0c0d011763ba5070ca6bc730e4
Matheus Paiva <MatheusPaivaBrito@outlook.com>
2020-02-21 13:19:13 -0300

    copy

--- server01.js
+++ server01.js
@@ -1,15 +1,19 @@
 const http = require('http')
 
 const server = http.createServer ((req, res) => {
-    const resp = []
-    resp['/'] = '<h1>Home</h1>'
-    resp['/contato'] = '<h1>Contato</h1>'
-
-    res.end(resp[req.url])
+    if(req.url == '/'){
+        res.end('<h1>Casa</h1>')
+        }
+    
+        if(req.url == '/Home'){
+        res.end('<h1>Sem Criatividade</h1>')
+        }
+    
+    res.end('<h1>Fora da casa</h1>')
 })
 
 server.listen(3001, 'localhost', () => {
     console.log('Servido de pé em http://localhost:3001')
     console.log('pra desligar o servidor : ctrl + c')
 })
-
+  
